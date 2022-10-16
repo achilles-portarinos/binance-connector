f3187ee408cf18c0c29fd3fb45b37b88d489d714
Achilles Portarinos <achilles.portarinos@zuehlke.com>
2022-10-16 19:15:52 +0200

    update

--- src/Model/Earning.cs
+++ src/Model/Earning.cs
@@ -1,11 +1,8 @@
-using System;
-
 namespace moderndrummer.binance.Model
 {
     public class Earning
     {
         public string Asset { get; set; }
         public decimal Amount { get; set; }
-        public DateTime? EarliestCreatedAt { get; set; }
     }
 }
